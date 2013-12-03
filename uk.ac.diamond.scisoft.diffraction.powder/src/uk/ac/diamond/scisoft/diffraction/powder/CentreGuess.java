b6710153b1911dac0fd37df995ee4bb5fee78567
Jacob Filik <jacob.filik@diamond.ac.uk>
2013-12-03 17:51:30 +0000

    Improvements to centre guess class

--- uk.ac.diamond.scisoft.diffraction.powder/src/uk/ac/diamond/scisoft/diffraction/powder/CentreGuess.java
+++ uk.ac.diamond.scisoft.diffraction.powder/src/uk/ac/diamond/scisoft/diffraction/powder/CentreGuess.java
@@ -6,19 +6,22 @@ import uk.ac.diamond.scisoft.analysis.dataset.Image;
 import uk.ac.diamond.scisoft.analysis.dataset.Signal;
 import uk.ac.diamond.scisoft.analysis.dataset.function.Downsample;
 import uk.ac.diamond.scisoft.analysis.dataset.function.DownsampleMode;
+import uk.ac.diamond.scisoft.analysis.fitting.Fitter;
+import uk.ac.diamond.scisoft.analysis.fitting.functions.CompositeFunction;
+import uk.ac.diamond.scisoft.analysis.fitting.functions.Gaussian;
+import uk.ac.diamond.scisoft.analysis.optimize.ApacheNelderMead;
 
 public class CentreGuess {
 	
 	public static double[] guessCentre(AbstractDataset image) {
 		
 		int downSample = 5;
-		//int samFW = 9;
-		//int samW = (samFW-1)/2;
+		int samFW = 36;
+		int samW = (samFW-1)/2;
 		
 		Downsample ds = new Downsample(DownsampleMode.MEAN, new int[]{downSample, downSample});
 		
 		AbstractDataset small = ds.value(image).get(0);
-		//AbstractDataset small = image.getSlice(null,new int[] {h,w},new int[] {5,5});
 		
 		small = DatasetUtils.cast(small, AbstractDataset.FLOAT64);
 		
@@ -28,43 +31,38 @@ public class CentreGuess {
 		
 		int[] maxPos = conv.maxPos();
 
-		//TODO robustly fit position - fitter doesnt seem to do this
-//		AbstractDataset patch = conv.getSlice(new int[] {maxPos[0] - samW-1, maxPos[1] - samW-1}, new int[] {maxPos[0] + samW, maxPos[1] + samW}, null);
-//		
-//		int[] shape = patch.getShape();
+		AbstractDataset patch = conv.getSlice(new int[] {maxPos[0] - samW-1, maxPos[1] - samW-1}, new int[] {maxPos[0] + samW, maxPos[1] + samW}, null);
 		
-		//NDGaussianFitResult res = Fitter.NDGaussianSimpleFit(patch, AbstractDataset.arange(shape[0], AbstractDataset.FLOAT64),AbstractDataset.arange(shape[1], AbstractDataset.FLOAT64));
-		//logger.info("max centre, x: " + res.getPos()[0] +" y: " + res.getPos()[1]);
+		AbstractDataset my = patch.sum(1);
+		AbstractDataset mx = patch.sum(0);
 		
-//		AbstractDataset my = patch.sum(1);
-//		AbstractDataset mx = patch.sum(0);
-//		
-//		AbstractDataset axis = AbstractDataset.arange(samFW, AbstractDataset.FLOAT64);
+		my.isubtract(my.min().doubleValue());
+		mx.isubtract(mx.min().doubleValue());
 		
-//		Gaussian g = new Gaussian(samFW/2, samFW/2, 1000);
-//		Offset off = new Offset(new double[]{my.min(true).doubleValue()});
-//		CompositeFunction func = new CompositeFunction();
-//		func.addFunction(g);
-//		func.addFunction(off);
-//		
-//		try {
-//			CompositeFunction out = Fitter.fit(axis, my, new ApacheNelderMead(), func);
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		AFunction yGauss = Fitter.GaussianFit(my, AbstractDataset.arange(samFW, AbstractDataset.FLOAT64));
-//		AFunction xGauss = Fitter.GaussianFit(mx, AbstractDataset.arange(samFW, AbstractDataset.FLOAT64));
-//		
-//		double convCenX = maxPos[1] - samW -1 + xGauss.getParameter(0).getValue();
-//		double convCenY = maxPos[0] - samW -1 + yGauss.getParameter(0).getValue();
+		AbstractDataset axis = AbstractDataset.arange(samFW, AbstractDataset.FLOAT64);
 		
-//		double convCenX = maxPos[1] - samW -1 + res.getPos()[1];
-//		double convCenY = maxPos[0] - samW -1 + res.getPos()[0];
+		Gaussian g = new Gaussian(samFW/2, samFW/4, my.max().doubleValue());
+		double yfound;
+		try {
+			CompositeFunction out = Fitter.fit(axis, my, new ApacheNelderMead(), g);
+			yfound = out.getFunction(0).getParameterValue(0);
+		} catch (Exception e) {
+			yfound = maxPos[1];
+			e.printStackTrace();
+		}
 		
-		double convCenX = maxPos[1];
-		double convCenY = maxPos[0];
+		g = new Gaussian(samFW/2, samFW/4, mx.max().doubleValue());
+		double xfound;
+		try {
+			CompositeFunction out = Fitter.fit(axis, mx, new ApacheNelderMead(), g);
+			xfound = out.getFunction(0).getParameterValue(0);
+		} catch (Exception e) {
+			xfound = maxPos[1];
+			e.printStackTrace();
+		}
+		
+		double convCenX = maxPos[1] - samW -1 + xfound;
+		double convCenY = maxPos[0] - samW -1 + yfound;
 		
 		double[] coOrds = new double[2];
 		
