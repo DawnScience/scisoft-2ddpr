8be3911b1d6839a8244e6cd3db0fb375e9ed697c
Jacob Filik <jacob.filik@diamond.ac.uk>
2013-12-05 16:48:11 +0000

    Ignores added for tdd test with hard coded paths to files

--- uk.ac.diamond.scisoft.diffraction.powder.test/src/uk/ac/diamond/scisoft/diffraction/powder/test/MultiImageCalibrationRunTest.java
+++ uk.ac.diamond.scisoft.diffraction.powder.test/src/uk/ac/diamond/scisoft/diffraction/powder/test/MultiImageCalibrationRunTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import uk.ac.diamond.scisoft.analysis.crystallography.CalibrantSpacing;
@@ -23,10 +24,10 @@ import uk.ac.diamond.scisoft.diffraction.powder.PowderCalibration;
 
 public class MultiImageCalibrationRunTest {
 	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
 	public void MultiImageTestPixium(){
 		
-	
 		AbstractDataset image;
 		try {
 			double pixel = 0.2;
@@ -35,7 +36,7 @@ public class MultiImageCalibrationRunTest {
 			CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
 			List<ResolutionEllipseROI> first = PowderCalibration.findMatchedEllipses(image, pixel, ceO2.getHKLs());
 			
-			assertEquals(197.97, first.get(0).getSemiAxis(0), 0.1);
+			//assertEquals(197.97, first.get(0).getSemiAxis(0), 0.1);
 			
 			image = (AbstractDataset)LoaderFactory.getDataSet("/dls/science/groups/das/ExampleData/i12/AlgorithmTesting/Circular1/pixi_00001.tif","image-01",null);
 			List<ResolutionEllipseROI> second = PowderCalibration.findMatchedEllipses(image, pixel, ceO2.getHKLs());
@@ -96,6 +97,7 @@ public class MultiImageCalibrationRunTest {
 		}
 	}
 	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
 	public void MultiImageTestPilatus(){
 		
@@ -167,6 +169,7 @@ public class MultiImageCalibrationRunTest {
 		}
 	}
 	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
 	public void MultiImageTestPerkin(){
 		
@@ -236,6 +239,101 @@ public class MultiImageCalibrationRunTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
+	@Test
+	public void MultiImageTestPerkinLab629k(){
+		
+		double pixel = 0.2;
+		AbstractDataset image = getImage("/dls/science/groups/das/ExampleData/i15/I15_Detector_Calibration/PE_Data/29p2keV/LaB6_29p2keV_d359-00021.tif");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
+		}
+		
+		CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
+		CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("Lab6Minimal6");
+		List<ResolutionEllipseROI> first = PowderCalibration.findMatchedEllipses(image, pixel, ceO2.getHKLs());
+		
+		
+		image = getImage("/dls/science/groups/das/ExampleData/i15/I15_Detector_Calibration/PE_Data/29p2keV/LaB6_29p2keV_d459-00022.tif");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
+		}
+		List<ResolutionEllipseROI> second = PowderCalibration.findMatchedEllipses(image, pixel, ceO2.getHKLs());
+		
+		image = getImage("/dls/science/groups/das/ExampleData/i15/I15_Detector_Calibration/PE_Data/29p2keV/LaB6_29p2keV_d559-00023.tif");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
+		}
+		List<ResolutionEllipseROI> third = PowderCalibration.findMatchedEllipses(image, pixel, ceO2.getHKLs());
+		
+		image = getImage("/dls/science/groups/das/ExampleData/i15/I15_Detector_Calibration/PE_Data/29p2keV/LaB6_29p2keV_d659-00024.tif");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
+		}
+		List<ResolutionEllipseROI> forth = PowderCalibration.findMatchedEllipses(image, pixel, ceO2.getHKLs());
+
+		double[] dDist = new double[]{350,450,550,650};
+		AbstractDataset deltaDistance = new DoubleDataset(dDist, dDist.length);
+
+		List<List<EllipticalROI>> allEllipses = new ArrayList<List<EllipticalROI>>();
+		allEllipses.add(new ArrayList<EllipticalROI>(first));
+		allEllipses.add(new ArrayList<EllipticalROI>(second));
+		allEllipses.add(new ArrayList<EllipticalROI>(third));
+		allEllipses.add(new ArrayList<EllipticalROI>(forth));
+
+		List<double[]> allDSpacings = new ArrayList<double[]>();
+
+		double[] fd = new double[first.size()];
+		int i = 0;
+		for (ResolutionEllipseROI r : first) {
+			fd[i++] = r.getResolution();
+		}
+		allDSpacings.add(fd);
+
+		fd = new double[second.size()];
+		i = 0;
+		for (ResolutionEllipseROI r : second) {
+			fd[i++] = r.getResolution();
+		}
+		allDSpacings.add(fd);
+
+		fd = new double[third.size()];
+		i = 0;
+		for (ResolutionEllipseROI r : third) {
+			fd[i++] = r.getResolution();
+		}
+		allDSpacings.add(fd);
+
+		fd = new double[forth.size()];
+		i = 0;
+		for (ResolutionEllipseROI r : forth) {
+			fd[i++] = r.getResolution();
+		}
+		allDSpacings.add(fd);
+
+		CalibrationOutput output = CalibrateEllipses.run(allEllipses, allDSpacings, deltaDistance, pixel);
+
+		assertEquals(0.425, output.getWavelength(), 0.0009);
+
+	}
+	
+	private AbstractDataset getImage(String path) {
+
+		AbstractDataset image = null;
+
+		try {
+			image = (AbstractDataset)LoaderFactory.getDataSet(path,"image-01",null);
+		} catch (Exception e) {
+			//fail silently, just return null to show it didnt work
+		}
+
+		return image;
 	}
 	
 }
--- uk.ac.diamond.scisoft.diffraction.powder.test/src/uk/ac/diamond/scisoft/diffraction/powder/test/PowerCalibrationKnownWavelengthTest.java
+++ uk.ac.diamond.scisoft.diffraction.powder.test/src/uk/ac/diamond/scisoft/diffraction/powder/test/PowerCalibrationKnownWavelengthTest.java
@@ -3,6 +3,7 @@ package uk.ac.diamond.scisoft.diffraction.powder.test;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import uk.ac.diamond.scisoft.analysis.crystallography.CalibrantSpacing;
@@ -17,119 +18,120 @@ import uk.ac.diamond.scisoft.diffraction.powder.PowderCalibration;
 
 public class PowerCalibrationKnownWavelengthTest {
 	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
 	public void CeO212kevPilatus170(){
 		
-		try {
-			AbstractDataset image = (AbstractDataset)LoaderFactory.getDataSet("/dls/science/groups/das/ExampleData/i04/CeO2/ceo2_12kev_170.cbf","image-01",null);
-			CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
-			CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
-			CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.97949, 0.172, ceO2.getHKLs());
-			
-			Assert.assertEquals(169.5, result.getDistance().getDouble(0), 0.9);
-			Assert.assertEquals(1210, result.getBeamCentreX().getDouble(0), 0.9);
-			Assert.assertEquals(1219.3, result.getBeamCentreY().getDouble(0), 0.9);
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		AbstractDataset image = getImage("/dls/science/groups/das/ExampleData/i04/CeO2/ceo2_12kev_170.cbf");
+
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
 		}
+		
+		CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
+		CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
+		CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.97949, 0.172, ceO2.getHKLs());
+		
+		Assert.assertEquals(169.5, result.getDistance().getDouble(0), 0.9);
+		Assert.assertEquals(1210, result.getBeamCentreX().getDouble(0), 0.9);
+		Assert.assertEquals(1219.3, result.getBeamCentreY().getDouble(0), 0.9);
 	}
 	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
 	public void CeO212kevPilatus370(){
 		
-		try {
-			AbstractDataset image = (AbstractDataset)LoaderFactory.getDataSet("/dls/science/groups/das/ExampleData/i04/CeO2/ceo2_12kev_370.cbf","image-01",null);
-			CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
-			CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
-			CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.97949, 0.172, ceO2.getHKLs());
-			
-			Assert.assertEquals(369.52, result.getDistance().getDouble(0), 0.9);
-			Assert.assertEquals(1214, result.getBeamCentreX().getDouble(0), 0.9);
-			Assert.assertEquals(1212.3, result.getBeamCentreY().getDouble(0), 0.9);
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		AbstractDataset image = getImage("/dls/science/groups/das/ExampleData/i04/CeO2/ceo2_12kev_370.cbf");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
 		}
+		
+		CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
+		CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
+		CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.97949, 0.172, ceO2.getHKLs());
+		
+		Assert.assertEquals(369.52, result.getDistance().getDouble(0), 0.9);
+		Assert.assertEquals(1214, result.getBeamCentreX().getDouble(0), 0.9);
+		Assert.assertEquals(1212.3, result.getBeamCentreY().getDouble(0), 0.9);
 	}
 	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
 	public void CeO216kevPilatus170(){
 		
-		try {
-			AbstractDataset image = (AbstractDataset)LoaderFactory.getDataSet("/dls/science/groups/das/ExampleData/i04/CeO2/ceo2_16kev_170.cbf","image-01",null);
-			CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
-			CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
-			CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.7749, 0.172, ceO2.getHKLs());
-			
-			Assert.assertEquals(169.5, result.getDistance().getDouble(0), 0.9);
-			Assert.assertEquals(1211.19, result.getBeamCentreX().getDouble(0), 0.9);
-			Assert.assertEquals(1219.15, result.getBeamCentreY().getDouble(0), 0.9);
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		AbstractDataset image = getImage("/dls/science/groups/das/ExampleData/i04/CeO2/ceo2_16kev_170.cbf");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
 		}
+		
+		CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
+		CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
+		CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.7749, 0.172, ceO2.getHKLs());
+		
+		Assert.assertEquals(169.5, result.getDistance().getDouble(0), 0.9);
+		Assert.assertEquals(1211.19, result.getBeamCentreX().getDouble(0), 0.9);
+		Assert.assertEquals(1219.15, result.getBeamCentreY().getDouble(0), 0.9);
 	}
 	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
 	public void CeO216kevPilatus370(){
 		//TODO needs ring rejection from fit to work
-		try {
-			AbstractDataset image = (AbstractDataset)LoaderFactory.getDataSet("/dls/science/groups/das/ExampleData/i04/CeO2/ceo2_16kev_370.cbf","image-01",null);
-			CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
-			CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
-			CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.7749, 0.172, ceO2.getHKLs());
-			
-			Assert.assertEquals(369.52, result.getDistance().getDouble(0), 0.9);
-			Assert.assertEquals(1215.38, result.getBeamCentreX().getDouble(0), 0.9);
-			Assert.assertEquals(1211.94, result.getBeamCentreY().getDouble(0), 0.9);
 		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		AbstractDataset image = getImage("/dls/science/groups/das/ExampleData/i04/CeO2/ceo2_16kev_370.cbf");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
 		}
+		
+		CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
+		CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
+		CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.7749, 0.172, ceO2.getHKLs());
+		
+		Assert.assertEquals(369.52, result.getDistance().getDouble(0), 0.9);
+		Assert.assertEquals(1215.38, result.getBeamCentreX().getDouble(0), 0.9);
+		Assert.assertEquals(1211.94, result.getBeamCentreY().getDouble(0), 0.9);
 	}
 
-	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
 	public void CeO229kevPerkin259(){
 		//TODO needs ring rejection from fit to work
-		try {
-			AbstractDataset image = (AbstractDataset)LoaderFactory.getDataSet("/dls/science/groups/das/ExampleData/i15/I15_Detector_Calibration/PE_Data/29p2keV/CeO2_29p2keV_d259-00017.tif","image-01",null);
-			CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
-			CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2Minimal");
-			CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.425, 0.2, ceO2.getHKLs());
-			
-			Assert.assertEquals(289, result.getDistance().getDouble(0), 0.9);
-			Assert.assertEquals(1050.7, result.getBeamCentreX().getDouble(0), 0.9);
-			Assert.assertEquals(1016.7, result.getBeamCentreY().getDouble(0), 0.9);
 		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		AbstractDataset image = getImage("/dls/science/groups/das/ExampleData/i15/I15_Detector_Calibration/PE_Data/29p2keV/CeO2_29p2keV_d259-00017.tif");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
 		}
+		
+		CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
+		CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
+		CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.425, 0.2, ceO2.getHKLs());
+		
+		Assert.assertEquals(289, result.getDistance().getDouble(0), 0.9);
+		Assert.assertEquals(1050.7, result.getBeamCentreX().getDouble(0), 0.9);
+		Assert.assertEquals(1016.7, result.getBeamCentreY().getDouble(0), 0.9);
 	}
 	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
 	public void CeO229kevPerkin659(){
-		//TODO needs ring rejection from fit to work
-		try {
-			AbstractDataset image = (AbstractDataset)LoaderFactory.getDataSet("/dls/science/groups/das/ExampleData/i15/I15_Detector_Calibration/PE_Data/29p2keV/CeO2_29p2keV_d659-00013.tif","image-01",null);
-			CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
-			CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
-			CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.425, 0.2, ceO2.getHKLs());
-			
-			Assert.assertEquals(689, result.getDistance().getDouble(0), 0.9);
-			Assert.assertEquals(1048, result.getBeamCentreX().getDouble(0), 0.9);
-			Assert.assertEquals(1015, result.getBeamCentreY().getDouble(0), 0.9);
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		AbstractDataset image = getImage("/dls/science/groups/das/ExampleData/i15/I15_Detector_Calibration/PE_Data/29p2keV/CeO2_29p2keV_d659-00013.tif");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
 		}
+		
+		CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
+		CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
+		CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.425, 0.2, ceO2.getHKLs());
+		
+		Assert.assertEquals(689, result.getDistance().getDouble(0), 0.9);
+		Assert.assertEquals(1048, result.getBeamCentreX().getDouble(0), 0.9);
+		Assert.assertEquals(1015, result.getBeamCentreY().getDouble(0), 0.9);
 	}
 	
 //	@Test
@@ -151,101 +153,114 @@ public class PowerCalibrationKnownWavelengthTest {
 //		}
 //	}
 	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
-	public void LaB6329kevPerkin259(){
+	public void LaB629kevPerkin359(){
 		//TODO needs ring rejection from fit to work
-		try {
-			AbstractDataset image = (AbstractDataset)LoaderFactory.getDataSet("/dls/science/groups/das/ExampleData/i15/I15_Detector_Calibration/PE_Data/29p2keV/LaB6_29p2keV_d359-00021.tif","image-01",null);
-			CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
-			CalibrantSpacing std = standards.getCalibrationPeakMap("LaB6");
-			CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.425, 0.2, std.getHKLs());
-			
-			Assert.assertEquals(390, result.getDistance().getDouble(0), 0.9);
-			Assert.assertEquals(1050, result.getBeamCentreX().getDouble(0), 0.9);
-			Assert.assertEquals(1016, result.getBeamCentreY().getDouble(0), 0.9);
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		AbstractDataset image = getImage("/dls/science/groups/das/ExampleData/i15/I15_Detector_Calibration/PE_Data/29p2keV/LaB6_29p2keV_d359-00021.tif");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
 		}
+		
+		CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
+		CalibrantSpacing std = standards.getCalibrationPeakMap("LaB6");
+		CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.425, 0.2, std.getHKLs());
+		
+		Assert.assertEquals(389.5, result.getDistance().getDouble(0), 0.9);
+		Assert.assertEquals(1050, result.getBeamCentreX().getDouble(0), 0.9);
+		Assert.assertEquals(1016, result.getBeamCentreY().getDouble(0), 0.9);
 	}
 	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
-	public void LaB6229kevPerkin659(){
-		try {
-			AbstractDataset image = (AbstractDataset)LoaderFactory.getDataSet("/dls/science/groups/das/ExampleData/i15/I15_Detector_Calibration/PE_Data/29p2keV/LaB6_29p2keV_d659-00024.tif","image-01",null);
-			CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
-			CalibrantSpacing std = standards.getCalibrationPeakMap("LaB6");
-			CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.425, 0.2, std.getHKLs());
-			
-			Assert.assertEquals(689, result.getDistance().getDouble(0), 0.9);
-			Assert.assertEquals(1048, result.getBeamCentreX().getDouble(0), 0.9);
-			Assert.assertEquals(1016, result.getBeamCentreY().getDouble(0), 0.9);
+	public void LaB629kevPerkin659(){
 		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		AbstractDataset image = getImage("/dls/science/groups/das/ExampleData/i15/I15_Detector_Calibration/PE_Data/29p2keV/LaB6_29p2keV_d659-00024.tif");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
 		}
+		
+		CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
+		CalibrantSpacing std = standards.getCalibrationPeakMap("LaB6");
+		CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.425, 0.2, std.getHKLs());
+		
+		Assert.assertEquals(689, result.getDistance().getDouble(0), 0.9);
+		Assert.assertEquals(1048, result.getBeamCentreX().getDouble(0), 0.9);
+		Assert.assertEquals(1016, result.getBeamCentreY().getDouble(0), 0.9);
+
 	}
 	
 	
-
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
 	public void CeO2Pixium788(){
 		//TODO needs ring rejection from fit to work
-		try {
-			AbstractDataset image = (AbstractDataset)LoaderFactory.getDataSet("/dls/science/groups/das/ExampleData/powder/ceopowder.tiff","image-01",null);
-			CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
-			CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
-			CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.2068, 0.148, ceO2.getHKLs());
-			
-			Assert.assertEquals(788.8, result.getDistance().getDouble(0), 0.9);
-			Assert.assertEquals(1441, result.getBeamCentreX().getDouble(0), 0.9);
-			Assert.assertEquals(1442.8, result.getBeamCentreY().getDouble(0), 0.9);
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		AbstractDataset image = getImage("/dls/science/groups/das/ExampleData/powder/ceopowder.tiff");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
 		}
+		
+		CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
+		CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
+		CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.2068, 0.148, ceO2.getHKLs());
+		
+		Assert.assertEquals(788.8, result.getDistance().getDouble(0), 0.9);
+		Assert.assertEquals(1441, result.getBeamCentreX().getDouble(0), 0.9);
+		Assert.assertEquals(1442.8, result.getBeamCentreY().getDouble(0), 0.9);
 	}
 	
 	
-	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
 	public void CeO2Pixium289(){
 		//TODO needs ring rejection from fit to work
-		try {
-			AbstractDataset image = (AbstractDataset)LoaderFactory.getDataSet("/dls/science/groups/das/ExampleData/i12/AlgorithmTesting/Circular1/pixi_00000.tif","image-01",null);
-			CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
-			CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2Minimal");
-			CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.425, 0.2, ceO2.getHKLs());
-			
-			Assert.assertEquals(289, result.getDistance().getDouble(0), 0.9);
-			Assert.assertEquals(1050.7, result.getBeamCentreX().getDouble(0), 0.9);
-			Assert.assertEquals(1016.7, result.getBeamCentreY().getDouble(0), 0.9);
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		AbstractDataset image = getImage("/dls/science/groups/das/ExampleData/i12/AlgorithmTesting/Circular1/pixi_00000.tif");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
 		}
+
+		CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
+		CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
+		CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.425, 0.2, ceO2.getHKLs());
+		
+		Assert.assertEquals(289, result.getDistance().getDouble(0), 0.9);
+		Assert.assertEquals(1050.7, result.getBeamCentreX().getDouble(0), 0.9);
+		Assert.assertEquals(1016.7, result.getBeamCentreY().getDouble(0), 0.9);
 	}
 	
+	@Ignore("Hard coded paths, currently for TDD, will be a valid test when we have a location for the files.")
 	@Test
 	public void CeO2Pixium689(){
 		//TODO needs ring rejection from fit to work
-		try {
-			AbstractDataset image = (AbstractDataset)LoaderFactory.getDataSet("/dls/science/groups/das/ExampleData/i12/AlgorithmTesting/Circular1/pixi_00004.tif","image-01",null);
-			CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
-			CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
-			CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.425, 0.2, ceO2.getHKLs());
-			
-			Assert.assertEquals(689, result.getDistance().getDouble(0), 0.9);
-			Assert.assertEquals(1048, result.getBeamCentreX().getDouble(0), 0.9);
-			Assert.assertEquals(1015.7, result.getBeamCentreY().getDouble(0), 0.9);
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		AbstractDataset image = getImage("/dls/science/groups/das/ExampleData/i12/AlgorithmTesting/Circular1/pixi_00004.tif");
+		if (image == null) {
+			Assert.fail("Image loading failed");
+			return;
 		}
+
+		CalibrationStandards standards = CalibrationFactory.getCalibrationStandards();
+		CalibrantSpacing ceO2 = standards.getCalibrationPeakMap("CeO2");
+		CalibrationOutput result = PowderCalibration.calibrateKnownWavelength(image, 0.425, 0.2, ceO2.getHKLs());
+		
+		Assert.assertEquals(689, result.getDistance().getDouble(0), 0.9);
+		Assert.assertEquals(1048, result.getBeamCentreX().getDouble(0), 0.9);
+		Assert.assertEquals(1015.7, result.getBeamCentreY().getDouble(0), 0.9);
+	}
+	
+	private AbstractDataset getImage(String path) {
+		
+		AbstractDataset image = null;
+		
+		try {
+			image = (AbstractDataset)LoaderFactory.getDataSet(path,"image-01",null);
+		} catch (Exception e) {
+			//fail silently, just return null to show it didnt work
+		}
+		
+		return image;
 	}
 }
