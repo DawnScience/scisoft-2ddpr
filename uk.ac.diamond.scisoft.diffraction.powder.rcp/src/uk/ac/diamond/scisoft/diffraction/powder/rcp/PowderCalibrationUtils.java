d607e130a80cfb4de809e5d52243032eb2149b30
Jacob Filik <jacob.filik@diamond.ac.uk>
2013-12-17 14:01:52 +0000

    Refactored out duplicate code, added single image e and d calibration

--- uk.ac.diamond.scisoft.diffraction.powder.rcp/src/uk/ac/diamond/scisoft/diffraction/powder/rcp/PowderCalibrationUtils.java
+++ uk.ac.diamond.scisoft.diffraction.powder.rcp/src/uk/ac/diamond/scisoft/diffraction/powder/rcp/PowderCalibrationUtils.java
@@ -29,7 +29,9 @@ import uk.ac.diamond.scisoft.analysis.diffraction.DetectorProperties;
 import uk.ac.diamond.scisoft.analysis.diffraction.DiffractionCrystalEnvironment;
 import uk.ac.diamond.scisoft.analysis.diffraction.PowderRingsUtils;
 import uk.ac.diamond.scisoft.analysis.diffraction.QSpace;
+import uk.ac.diamond.scisoft.analysis.diffraction.ResolutionEllipseROI;
 import uk.ac.diamond.scisoft.analysis.io.IDiffractionMetadata;
+import uk.ac.diamond.scisoft.analysis.monitor.IMonitor;
 import uk.ac.diamond.scisoft.analysis.roi.CircularROI;
 import uk.ac.diamond.scisoft.analysis.roi.EllipticalROI;
 import uk.ac.diamond.scisoft.analysis.roi.IROI;
@@ -362,6 +364,67 @@ public class PowderCalibrationUtils {
 	private static final int MAX_RINGS = 10;
 	private static final int CENTRE_MASK_RADIUS = 50;
 	
+	public static Job autoFindEllipsesAllSingleImage(final Display display,
+			final IPlottingSystem plottingSystem,
+			final DiffractionTableData currentData,
+			final int maxRings) {
+
+		Job job = new Job("Calibrate detector") {
+			@Override
+			protected IStatus run(final IProgressMonitor monitor) {
+				
+				final AbstractDataset image = (AbstractDataset)currentData.image;
+				IDiffractionMetadata meta = currentData.md;
+				
+				final EllipseFindingStructure efs = getResolutionEllipses(image, meta, maxRings, monitor);
+				
+				if (monitor.isCanceled() || efs == null) return Status.CANCEL_STATUS;
+				
+				List<ResolutionEllipseROI> foundEllipses = getFittedResolutionROIs(plottingSystem, efs, display, monitor);
+				
+				if (monitor.isCanceled() || foundEllipses == null) return Status.CANCEL_STATUS;
+				
+				List<List<EllipticalROI>> allEllipses = new ArrayList<List<EllipticalROI>>();
+				allEllipses.add(new ArrayList<EllipticalROI>(foundEllipses));
+				
+				double pixelSize = currentData.md.getDetector2DProperties().getHPxSize();
+				
+				List<double[]> allDSpacings = new ArrayList<double[]>();
+				double[] dSpaceArray = new double[foundEllipses.size()];
+				
+				for (int j = 0; j < foundEllipses.size();j++) {
+					dSpaceArray[j] = foundEllipses.get(j).getResolution();
+				}
+				
+				double[] dDist = new double[]{0};
+				AbstractDataset deltaDistance = new DoubleDataset(dDist, dDist.length);
+				allDSpacings.add(dSpaceArray);
+				monitor.beginTask("Calibrating", IProgressMonitor.UNKNOWN);
+				final CalibrationOutput output = CalibrateEllipses.run(allEllipses, allDSpacings,deltaDistance, pixelSize);
+				
+				display.syncExec(new Runnable() {
+					@Override
+					public void run() {
+						DetectorProperties dp = currentData.md.getDetector2DProperties();
+						currentData.md.getDiffractionCrystalEnvironment().setWavelength(output.getWavelength());
+						dp.setBeamCentreDistance(output.getDistance().getDouble(0));
+						double[] bc = new double[] {output.getBeamCentreX().getDouble(0),output.getBeamCentreY().getDouble(0) };
+						dp.setBeamCentreCoords(bc);
+
+						dp.setNormalAnglesInDegrees(output.getTilt().getDouble(0)*-1, 0, output.getTiltAngle().getDouble(0)*-1);
+
+						hideFoundRings(plottingSystem);
+						drawCalibrantRings(currentData.augmenter);
+					}
+				});
+
+				return Status.OK_STATUS;
+			};
+		};
+		job.setPriority(Job.SHORT);
+		return job;
+	}
+	
 	public static Job autoFindEllipses(final Display display,
 			final IPlottingSystem plottingSystem,
 			final DiffractionTableData currentData,
@@ -373,112 +436,26 @@ public class PowderCalibrationUtils {
 				
 				final AbstractDataset image = (AbstractDataset)currentData.image;
 				IDiffractionMetadata meta = currentData.md;
-				int[] shape = image.getShape();
-				monitor.beginTask("Finding approximate centre...", IProgressMonitor.UNKNOWN);
-				double[] approxCentre = CentreGuess.guessCentre(image);
 				
-				double[] farCorner = new double[]{0,0};
-				if (approxCentre[0] < shape[0]/2.0) farCorner[0] = shape[0];
-				if (approxCentre[1] < shape[1]/2.0) farCorner[1] = shape[1];
-				double maxDistance = Math.sqrt(Math.pow(approxCentre[0]-farCorner[0],2)+Math.pow(approxCentre[1]-farCorner[1],2));
-				SectorROI sector = new SectorROI(approxCentre[0], approxCentre[1], 0, maxDistance, 0, 2*Math.PI);
-				monitor.beginTask("Integrating image...", IProgressMonitor.UNKNOWN);
-				AbstractDataset[] profile = ROIProfile.sector(image, null, sector, true, false, false, null, XAxis.PIXEL, false);
-				if (monitor.isCanceled()) return Status.CANCEL_STATUS;
-				final AbstractDataset y = profile[0];
+				final EllipseFindingStructure efs = getResolutionEllipses(image, meta, maxRings, monitor);
 				
-				for (int i = 0 ; i < CENTRE_MASK_RADIUS ; i++) {
-					y.set(0, i);
-				}
+				if (monitor.isCanceled() || efs == null) return Status.CANCEL_STATUS;
 				
-				final AbstractDataset x = AbstractDataset.arange(y.getSize(), AbstractDataset.INT32);
+				List<ResolutionEllipseROI> foundEllipses = getFittedResolutionROIs(plottingSystem, efs, display, monitor);
 				
-				List<HKL> spacings = CalibrationFactory.getCalibrationStandards().getCalibrant().getHKLs();
-				
-				int max = spacings.size() > maxRings ? maxRings : spacings.size();
-				
-				double[] dSpace = new double[max];
-				
-				for (int i = 0; i < max; i++) dSpace[i] = spacings.get(i).getDNano()*10;
-				monitor.beginTask("Matching to standard...", IProgressMonitor.UNKNOWN);
-				final Map<Double,Double> dSpaceRadiusMap = BruteStandardMatcher.bruteForceMatchStandards(x, y, dSpace, meta.getDetector2DProperties().getHPxSize());
-				
-				if (monitor.isCanceled()) return Status.CANCEL_STATUS;
-				
-				final List<EllipticalROI> ellipses = new ArrayList<EllipticalROI>();
-				
-				double[] inner = new double[dSpace.length];
-				double[] outer = new double[dSpace.length];
-				
-				for (int i = 0; i < dSpace.length; i++) {
-					//TODO if dSpace == 1;
-					double dVal = dSpaceRadiusMap.get(dSpace[i]);
-					ellipses.add(new EllipticalROI(dVal, approxCentre[0],approxCentre[1]));
-					inner.toString();
-					if (i == 0) {
-						double out = (dSpaceRadiusMap.get(dSpace[i+1]) - dVal)/2;
-						inner[i] = out > 50 ? 50 : out;
-						outer[i] = out > 50 ? 50 : out;
-						
-					} else if (i == dSpace.length -1) {
-						double in = (dVal - dSpaceRadiusMap.get(dSpace[i-1]))/2;
-						inner[i] = in > 50 ? 50 : in;
-						outer[i] = in > 50 ? 50 : in;
-					} else {
-						double in = (dVal - dSpaceRadiusMap.get(dSpace[i-1]))/2;
-						double out = (dSpaceRadiusMap.get(dSpace[i+1]) - dVal)/2;
-						inner[i] = in > 50 ? 50 : in;;
-						outer[i] = out > 50 ? 50 : out;;
-					}
-					
-				}
-				monitor.beginTask("Finding ellipses", IProgressMonitor.UNKNOWN);
-				final List<EllipticalROI> foundEllipses = new ArrayList<EllipticalROI>();
-				if (monitor.isCanceled()) return Status.CANCEL_STATUS;
-				IROI roi = null;
-				double corFact = 0;
-				double lastAspect = 1;
-				double lastAngle = 0;
-				List<Double> dList = new ArrayList<Double>();
-				int i = 0;
-				for (EllipticalROI e : ellipses) {
-					
-					double startSemi = e.getSemiAxis(0);
-					e.setSemiAxis(0, startSemi+corFact);
-					e.setSemiAxis(1, (startSemi+corFact)/lastAspect);
-					e.setAngle(lastAngle);
-					//e.set
-					
-					IImageTrace t = DiffractionCalibrationUtils.getImageTrace(plottingSystem);
-					try {
-						roi = DiffractionTool.runEllipsePeakFit(monitor, display, plottingSystem, t, e, inner[i], outer[i],256);
-					} catch (Exception ex) {
-						logger.debug(ex.getMessage());
-						roi = null;
-					} 
-					if (monitor.isCanceled()) return Status.CANCEL_STATUS;
-					if (roi != null) {
-						foundEllipses.add((EllipticalROI)roi);
-						dList.add(dSpace[i]);
-						corFact = ((EllipticalROI)roi).getSemiAxis(0) - startSemi;
-						lastAspect = ((EllipticalROI) roi).getAspectRatio();
-						lastAngle = ((EllipticalROI) roi).getAngle();
-						DiffractionCalibrationUtils.drawFoundRing(monitor, display, plottingSystem, roi, false);
-					}
-					i++;
-				}
+				if (monitor.isCanceled() || foundEllipses == null) return Status.CANCEL_STATUS;
 				
 				List<List<EllipticalROI>> allEllipses = new ArrayList<List<EllipticalROI>>();
-				allEllipses.add(foundEllipses);
+				allEllipses.add(new ArrayList<EllipticalROI>(foundEllipses));
 				
 				double pixelSize = currentData.md.getDetector2DProperties().getHPxSize();
 				double wavelength = currentData.md.getDiffractionCrystalEnvironment().getWavelength();
 				
 				List<double[]> allDSpacings = new ArrayList<double[]>();
-				double[] dSpaceArray = new double[dList.size()];
+				double[] dSpaceArray = new double[foundEllipses.size()];
 				
-				for (int j = 0; j < dList.size();j++) {
-					dSpaceArray[j] = dList.get(j);
+				for (int j = 0; j < foundEllipses.size();j++) {
+					dSpaceArray[j] = foundEllipses.get(j).getResolution();
 				}
 				
 				allDSpacings.add(dSpaceArray);
@@ -533,108 +510,26 @@ public class PowderCalibrationUtils {
 					
 					final AbstractDataset image = (AbstractDataset)data.image;
 					IDiffractionMetadata meta = data.md;
-					int[] shape = image.getShape();
-					monitor.beginTask("Finding approximate centre...", IProgressMonitor.UNKNOWN);
-					double[] approxCentre = CentreGuess.guessCentre(image);
 					
-					double[] farCorner = new double[]{0,0};
-					if (approxCentre[0] < shape[0]/2.0) farCorner[0] = shape[0];
-					if (approxCentre[1] < shape[1]/2.0) farCorner[1] = shape[1];
-					double maxDistance = Math.sqrt(Math.pow(approxCentre[0]-farCorner[0],2)+Math.pow(approxCentre[1]-farCorner[1],2));
-					SectorROI sector = new SectorROI(approxCentre[0], approxCentre[1], 0, maxDistance, 0, 2*Math.PI);
-					monitor.beginTask("Integrating image...", IProgressMonitor.UNKNOWN);
-					AbstractDataset[] profile = ROIProfile.sector(image, null, sector, true, false, false, null, XAxis.PIXEL, false);
-					if (monitor.isCanceled()) return Status.CANCEL_STATUS;
-					final AbstractDataset y = profile[0];
-					
-					for (int i = 0 ; i < CENTRE_MASK_RADIUS ; i++) {
-						y.set(0, i);
-					}
-					
-					final AbstractDataset x = AbstractDataset.arange(y.getSize(), AbstractDataset.INT32);
-					
-					List<HKL> spacings = CalibrationFactory.getCalibrationStandards().getCalibrant().getHKLs();
-					
-					int max = spacings.size() > maxRings ? maxRings : spacings.size();
-					
-					double[] dSpace = new double[max];
-					
-					for (int i = 0; i < max; i++) dSpace[i] = spacings.get(i).getDNano()*10;
-					monitor.beginTask("Matching to standard...", IProgressMonitor.UNKNOWN);
-					final Map<Double,Double> dSpaceRadiusMap = BruteStandardMatcher.bruteForceMatchStandards(x, y, dSpace, meta.getDetector2DProperties().getHPxSize());
+					final EllipseFindingStructure efs = getResolutionEllipses(image, meta, maxRings, monitor);
 					
 					if (monitor.isCanceled()) return Status.CANCEL_STATUS;
 					
-					final List<EllipticalROI> ellipses = new ArrayList<EllipticalROI>();
+					if (efs == null) return Status.CANCEL_STATUS;
 					
-					double[] inner = new double[dSpace.length];
-					double[] outer = new double[dSpace.length];
+					List<ResolutionEllipseROI> foundEllipses = getFittedResolutionROIs(plottingSystem, efs, display, monitor);
 					
-					for (int i = 0; i < dSpace.length; i++) {
-						//TODO out might be known if max less than spacings.size();
-						double dVal = dSpaceRadiusMap.get(dSpace[i]);
-						ellipses.add(new EllipticalROI(dVal, approxCentre[0],approxCentre[1]));
-						inner.toString();
-						if (i == 0) {
-							double out = (dSpaceRadiusMap.get(dSpace[i+1]) - dVal)/2;
-							inner[i] = out > 50 ? 50 : out;
-							outer[i] = out > 50 ? 50 : out;
-							
-						} else if (i == dSpace.length -1) {
-							double in = (dVal - dSpaceRadiusMap.get(dSpace[i-1]))/2;
-							inner[i] = in > 50 ? 50 : in;
-							outer[i] = in > 50 ? 50 : in;
-						} else {
-							double in = (dVal - dSpaceRadiusMap.get(dSpace[i-1]))/2;
-							double out = (dSpaceRadiusMap.get(dSpace[i+1]) - dVal)/2;
-							inner[i] = in > 50 ? 50 : in;;
-							outer[i] = out > 50 ? 50 : out;;
-						}
-						
-					}
-					monitor.beginTask("Finding ellipses", IProgressMonitor.UNKNOWN);
-					final List<EllipticalROI> foundEllipses = new ArrayList<EllipticalROI>();
-					if (monitor.isCanceled()) return Status.CANCEL_STATUS;
-					IROI roi = null;
-					double corFact = 0;
-					double lastAspect = 1;
-					double lastAngle = 0;
-					List<Double> dList = new ArrayList<Double>();
-					int i = 0;
-					for (EllipticalROI e : ellipses) {
-						
-						double startSemi = e.getSemiAxis(0);
-						e.setSemiAxis(0, startSemi+corFact);
-						e.setSemiAxis(1, (startSemi+corFact)/lastAspect);
-						e.setAngle(lastAngle);
-						//e.set
-						
-						IImageTrace t = DiffractionCalibrationUtils.getImageTrace(plottingSystem);
-						try {
-							roi = DiffractionTool.runEllipsePeakFit(monitor, display, plottingSystem, t, e, inner[i], outer[i],384);
-						} catch (Exception ex) {
-							logger.debug(ex.getMessage());
-							roi = null;
-						} 
-						if (monitor.isCanceled()) return Status.CANCEL_STATUS;
-						if (roi != null) {
-							foundEllipses.add((EllipticalROI)roi);
-							dList.add(dSpace[i]);
-							corFact = ((EllipticalROI)roi).getSemiAxis(0) - startSemi;
-							lastAspect = ((EllipticalROI) roi).getAspectRatio();
-							lastAngle = ((EllipticalROI) roi).getAngle();
-							DiffractionCalibrationUtils.drawFoundRing(monitor, display, plottingSystem, roi, false);
-						}
-						i++;
-					}
-					double[] dSpaceArray = new double[dList.size()];
+					if (monitor.isCanceled() || foundEllipses == null) return Status.CANCEL_STATUS;
 					
-					for (int j = 0; j < dList.size();j++) {
-						dSpaceArray[j] = dList.get(j);
+					
+					double[] dSpaceArray = new double[foundEllipses.size()];
+					
+					for (int j = 0; j < foundEllipses.size();j++) {
+						dSpaceArray[j] = foundEllipses.get(j).getResolution();
 					}
 					
 					allDSpacings.add(dSpaceArray);
-					allEllipses.add(foundEllipses);
+					allEllipses.add(new ArrayList<EllipticalROI>(foundEllipses));
 					
 					display.syncExec(new Runnable() {
 						@Override
@@ -676,5 +571,120 @@ public class PowderCalibrationUtils {
 		};
 		job.setPriority(Job.SHORT);
 		return job;
+	}
+	
+	private static List<ResolutionEllipseROI> getFittedResolutionROIs(IPlottingSystem plottingSystem, EllipseFindingStructure efs,
+			Display display, IProgressMonitor monitor) {
+		monitor.beginTask("Finding ellipses", IProgressMonitor.UNKNOWN);
+		final List<ResolutionEllipseROI> foundEllipses = new ArrayList<ResolutionEllipseROI>();
+		if (monitor.isCanceled()) return null;
+		IROI roi = null;
+		double corFact = 0;
+		double lastAspect = 1;
+		double lastAngle = 0;
+		int i = 0;
+		for (ResolutionEllipseROI e : efs.ellipses) {
+			
+			double startSemi = e.getSemiAxis(0);
+			e.setSemiAxis(0, startSemi+corFact);
+			e.setSemiAxis(1, (startSemi+corFact)/lastAspect);
+			e.setAngle(lastAngle);
+			//e.set
+			
+			IImageTrace t = DiffractionCalibrationUtils.getImageTrace(plottingSystem);
+			try {
+				roi = DiffractionTool.runEllipsePeakFit(monitor, display, plottingSystem, t, e, efs.innerSearch[i], efs.outerSearch[i],256);
+			} catch (Exception ex) {
+				logger.debug(ex.getMessage());
+				roi = null;
+			} 
+			if (monitor.isCanceled()) return null;
+			if (roi != null) {
+				foundEllipses.add(new ResolutionEllipseROI((EllipticalROI)roi, e.getResolution()));
+				corFact = ((EllipticalROI)roi).getSemiAxis(0) - startSemi;
+				lastAspect = ((EllipticalROI) roi).getAspectRatio();
+				lastAngle = ((EllipticalROI) roi).getAngle();
+				DiffractionCalibrationUtils.drawFoundRing(monitor, display, plottingSystem, roi, false);
+			}
+			i++;
+		}
+		
+		return foundEllipses;
+	}
+	
+	private static EllipseFindingStructure getResolutionEllipses(AbstractDataset image, IDiffractionMetadata meta, int maxRings, IProgressMonitor monitor) {
+		int[] shape = image.getShape();
+		monitor.beginTask("Finding approximate centre...", IProgressMonitor.UNKNOWN);
+		double[] approxCentre = CentreGuess.guessCentre(image);
+		
+		double[] farCorner = new double[]{0,0};
+		if (approxCentre[0] < shape[0]/2.0) farCorner[0] = shape[0];
+		if (approxCentre[1] < shape[1]/2.0) farCorner[1] = shape[1];
+		double maxDistance = Math.sqrt(Math.pow(approxCentre[0]-farCorner[0],2)+Math.pow(approxCentre[1]-farCorner[1],2));
+		SectorROI sector = new SectorROI(approxCentre[0], approxCentre[1], 0, maxDistance, 0, 2*Math.PI);
+		monitor.beginTask("Integrating image...", IProgressMonitor.UNKNOWN);
+		AbstractDataset[] profile = ROIProfile.sector(image, null, sector, true, false, false, null, XAxis.PIXEL, false);
+		if (monitor.isCanceled()) return null;
+		final AbstractDataset y = profile[0];
+		
+		for (int i = 0 ; i < CENTRE_MASK_RADIUS ; i++) {
+			y.set(0, i);
+		}
+		
+		final AbstractDataset x = AbstractDataset.arange(y.getSize(), AbstractDataset.INT32);
+		
+		List<HKL> spacings = CalibrationFactory.getCalibrationStandards().getCalibrant().getHKLs();
+		
+		int max = spacings.size() > maxRings ? maxRings : spacings.size();
+		
+		double[] dSpace = new double[max];
+		
+		for (int i = 0; i < max; i++) dSpace[i] = spacings.get(i).getDNano()*10;
+		monitor.beginTask("Matching to standard...", IProgressMonitor.UNKNOWN);
+		final Map<Double,Double> dSpaceRadiusMap = BruteStandardMatcher.bruteForceMatchStandards(x, y, dSpace, meta.getDetector2DProperties().getHPxSize());
+		
+		if (monitor.isCanceled()) return null;
+		
+		final List<ResolutionEllipseROI> ellipses = new ArrayList<ResolutionEllipseROI>();
+		
+		double[] inner = new double[dSpace.length];
+		double[] outer = new double[dSpace.length];
+		
+		for (int i = 0; i < dSpace.length; i++) {
+			//TODO out might be known if max less than spacings.size();
+			double dVal = dSpaceRadiusMap.get(dSpace[i]);
+			EllipticalROI el = new EllipticalROI(dVal, approxCentre[0],approxCentre[1]);
+			ellipses.add(new ResolutionEllipseROI(el, dSpace[i]));
+			inner.toString();
+			if (i == 0) {
+				double out = (dSpaceRadiusMap.get(dSpace[i+1]) - dVal)/2;
+				inner[i] = out > 50 ? 50 : out;
+				outer[i] = out > 50 ? 50 : out;
+				
+			} else if (i == dSpace.length -1) {
+				double in = (dVal - dSpaceRadiusMap.get(dSpace[i-1]))/2;
+				inner[i] = in > 50 ? 50 : in;
+				outer[i] = in > 50 ? 50 : in;
+			} else {
+				double in = (dVal - dSpaceRadiusMap.get(dSpace[i-1]))/2;
+				double out = (dSpaceRadiusMap.get(dSpace[i+1]) - dVal)/2;
+				inner[i] = in > 50 ? 50 : in;;
+				outer[i] = out > 50 ? 50 : out;;
+			}
+		}
+		
+		EllipseFindingStructure efs = new EllipseFindingStructure();
+		efs.ellipses = ellipses;
+		efs.innerSearch = inner;
+		efs.outerSearch = outer;
+		
+		return efs;
+		
+	}
+	
+	private static class EllipseFindingStructure {
+		List<ResolutionEllipseROI> ellipses;
+		double[] innerSearch;
+		double[] outerSearch;
 	}
 }
