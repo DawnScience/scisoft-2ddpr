c7fd6b9ea80bc1de5a0fbfd1f133bb1776aa7403
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2014-01-10 14:33:17 +0000

    powder diffcal: Remove unused view and perspective

--- uk.ac.diamond.scisoft.diffraction.powder.rcp/src/uk/ac/diamond/scisoft/diffraction/powder/rcp/DiffractionCalibrationPerspective.java
+++ uk.ac.diamond.scisoft.diffraction.powder.rcp/src/uk/ac/diamond/scisoft/diffraction/powder/rcp/DiffractionCalibrationPerspective.java
@@ -4,41 +4,47 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+import uk.ac.diamond.scisoft.diffraction.powder.rcp.views.DiffractionCalibrationView;
+import uk.ac.diamond.scisoft.diffraction.powder.rcp.views.DiffractionPlotView;
+
 public class DiffractionCalibrationPerspective implements IPerspectiveFactory {
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
 		layout.setEditorAreaVisible(false);
-		
-		layout.setFixed(true);
-		
+	
 		IFolderLayout navigatorFolder = layout.createFolder("navigator-folder", IPageLayout.LEFT, 0.15f, editorArea);
 		navigatorFolder.addView("org.eclipse.ui.navigator.ProjectExplorer");
 		navigatorFolder.addView("uk.ac.diamond.sda.navigator.views.FileView");
-		{
-			IFolderLayout folderLayout = layout.createFolder("folder", IPageLayout.LEFT, 0.8f, IPageLayout.ID_EDITOR_AREA);
-			folderLayout.addView("uk.ac.diamond.scisoft.diffraction.powder.rcp.calibrationview");
-		}
-		
-		
-		
-		
-//		{
-//			IFolderLayout folderLayout = layout.createFolder("folder", IPageLayout.LEFT, 0.4f, IPageLayout.ID_EDITOR_AREA);
-//			folderLayout.addView("uk.ac.diamond.scisoft.diffraction.powder.rcp.calibrationplot");
-//			IViewLayout vLayout = layout.getViewLayout("uk.ac.diamond.scisoft.diffraction.powder.rcp.calibrationplot");
-//			vLayout.setCloseable(false);
-//		}
-//		
-//		IFolderLayout toolPageLayout = layout.createFolder("toolPageFolder", IPageLayout.RIGHT, 0.4f, "uk.ac.diamond.scisoft.diffraction.powder.rcp.calibrationplot");
-//		toolPageLayout.addView("org.dawb.workbench.plotting.views.toolPageView.fixed:org.dawb.workbench.plotting.tools.diffraction.Diffraction");
-//		toolPageLayout.addPlaceholder("*");
-//		
-//		toolPageLayout = layout.createFolder("toolPageFolder2", IPageLayout.BOTTOM, 0.4f, "org.dawb.workbench.plotting.views.toolPageView.fixed:org.dawb.workbench.plotting.tools.diffraction.Diffraction");
-//		toolPageLayout.addView("org.dawb.workbench.plotting.views.toolPageView.fixed:org.dawnsci.plotting.tools.powdercheck");
-//		toolPageLayout.addPlaceholder("*");
-		
+
+		String fixed = "org.dawb.workbench.plotting.views.toolPageView.fixed:";
+		String powderCheckID = "org.dawnsci.plotting.tools.powdercheck";
+		String diffractionID = "uk.ac.diamond.scisoft.diffraction.powder.rcp.powderDiffractionTool";
+
+		// Top left: Diffraction calibration view
+		IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.RIGHT, 0.25f, editorArea);
+		topLeft.addView(DiffractionCalibrationView.ID);
+
+		// Top right: Diffraction plotting view
+		IFolderLayout topRight = layout.createFolder("topRight", IPageLayout.RIGHT, 0.50f, "topLeft");
+		topRight.addView(DiffractionPlotView.ID);
+
+		// Bottom left: Diffraction tool view
+		IFolderLayout bottomLeft = layout.createFolder("bottomLeft", IPageLayout.BOTTOM, 0.50f, "topLeft");
+		// open the tool as a fixed view
+		bottomLeft.addView(fixed + diffractionID);
+
+		// Bottom Right: Powder diffraction tool
+		IFolderLayout bottomRight = layout.createFolder("bottomRight", IPageLayout.BOTTOM, 0.50f, "topRight");
+		// open the tool as a fixed view
+		bottomRight.addView(fixed + powderCheckID);
+
+		layout.getViewLayout(DiffractionPlotView.ID).setCloseable(false);
+		layout.getViewLayout(DiffractionCalibrationView.ID).setCloseable(false);
+		layout.getViewLayout(fixed + diffractionID).setCloseable(false);
+		layout.getViewLayout(fixed + powderCheckID).setCloseable(false);
+
 	}
 
 }
