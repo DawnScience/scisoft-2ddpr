d88fa16df631139acfa30eab33ed1d68ab3f5eed
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-05-12 11:28:01 +0100

    Simplify code

--- uk.ac.diamond.scisoft.diffraction.powder.rcp/src/uk/ac/diamond/scisoft/diffraction/powder/rcp/DiffractionCalibrationPerspectiveLaunch.java
+++ uk.ac.diamond.scisoft.diffraction.powder.rcp/src/uk/ac/diamond/scisoft/diffraction/powder/rcp/DiffractionCalibrationPerspectiveLaunch.java
@@ -1,38 +1,11 @@
 package uk.ac.diamond.scisoft.diffraction.powder.rcp;
 
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.WorkbenchException;
+import org.dawb.common.ui.perspective.AbstractPerspectiveLaunch;
 
-public class DiffractionCalibrationPerspectiveLaunch implements
-	IWorkbenchWindowActionDelegate {
+public class DiffractionCalibrationPerspectiveLaunch extends AbstractPerspectiveLaunch {
 
 	@Override
-	public void run(IAction action) {
-		try {
-			PlatformUI.getWorkbench().showPerspective(DiffractionCalibrationPerspective.ID,PlatformUI.getWorkbench().getActiveWorkbenchWindow());
-		} catch (WorkbenchException e) {
-			e.printStackTrace();
-		}
-		
+	public String getID() {
+		return DiffractionCalibrationPerspective.ID;
 	}
-
-	@Override
-	public void selectionChanged(IAction action, ISelection selection) {
-		
-	}
-
-	@Override
-	public void dispose() {
-		
-	}
-
-	@Override
-	public void init(IWorkbenchWindow window) {
-		
-	}
-
 }
