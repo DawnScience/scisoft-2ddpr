bd4f41a38f514da935fe613dfba58c916d3d71e9
Tim Spain <timothy.spain@diamond.ac.uk>
2016-08-08 11:23:19 +0100

    Add (dummy) content to the grating part of the grating calibration dialog

--- uk.ac.diamond.scisoft.diffraction.powder.rcp/src/uk/ac/diamond/scisoft/diffraction/powder/rcp/wizards/GratingCalibrationDialog.java
+++ uk.ac.diamond.scisoft.diffraction.powder.rcp/src/uk/ac/diamond/scisoft/diffraction/powder/rcp/wizards/GratingCalibrationDialog.java
@@ -26,7 +26,9 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 import uk.ac.diamond.scisoft.diffraction.powder.rcp.table.DiffractionDataManager;
 
@@ -35,6 +37,9 @@ public class GratingCalibrationDialog extends Dialog {
 	private DiffractionDataManager manager;
 	private IPlottingSystem<Composite> plotSystem;
 	private IToolPage toolPage;
+	private Text rulingText;
+	private Label spacingText;
+	private Label angleText;
 	
 	public GratingCalibrationDialog(Shell shell, DiffractionDataManager manager) {
 		super(shell);
@@ -92,14 +97,42 @@ public class GratingCalibrationDialog extends Dialog {
 
 		// Add a composite to hold the grating information
 		Composite gratingCompo = new Composite(dataColumn, SWT.FILL | SWT.BORDER);
-		
+		gratingCompo.setLayout(new GridLayout(4, true));
+		gratingCompo.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		Label rulingLabel = new Label(gratingCompo, SWT.BORDER);
+		rulingLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+		rulingLabel.setText("Grating ruling spacing");
+		rulingText = new Text(gratingCompo, SWT.BORDER);
+		rulingText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		rulingText.setText("blah");
+		Label rulingUnitLabel = new Label(gratingCompo, SWT.BORDER);
+		rulingUnitLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		rulingUnitLabel.setText("nm");
+		Label spacingLabel = new Label(gratingCompo, SWT.BORDER);
+		spacingLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+		spacingLabel.setText("Fringe spacing");
+		spacingText = new Label(gratingCompo, SWT.BORDER);
+		spacingText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		spacingText.setText("blah");
+		Label spacingUnitLabel = new Label(gratingCompo, SWT.BORDER);
+		spacingUnitLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		spacingUnitLabel.setText("px");
+		Label angleLabel = new Label(gratingCompo, SWT.BORDER);
+		angleLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+		angleLabel.setText("Grating angle");
+		angleText = new Label(gratingCompo, SWT.BORDER);
+		angleText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		angleText.setText("blah");
+		Label angleUnitLabel = new Label(gratingCompo, SWT.BORDER);
+		angleUnitLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		angleUnitLabel.setText("°");
 		
 		return container;
 	}
 	
 	@Override
 	protected Point getInitialSize() {
-		return new Point(1000, 750);
+		return new Point(1000, 700);
 	}
 	
 	@Override
